00888f34836213d839c2baba84d7bb8905022917
Jack Farrington <jack.farrington@gmail.com>
2026-01-09 19:43:33 -0600

    Cosmetic changes.

--- components/supply-summary.tsx
+++ components/supply-summary.tsx
@@ -6,75 +6,85 @@ export default async function SupplySummary() {
     const { symbol, mintedSupply, burnedSoFar, remainingSupply, pendingBurn, totalBurned, circulation, locked, power, emittedSupply, lastUpdate } = await getMetrics();
 
   return (
-    <>
-      <p className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-        {symbol}
-      </p>
+    <div className="grid gap-6">
+      <div className="rounded-xl bg-purple-50 p-2 shadow-sm">
+        <div className="flex justify-center">
+          <h3 className="text-lg font-medium">{symbol}</h3>
+        </div>
 
-      <table>
-        <tbody>
-          <tr>
-            <td>Minted</td>
-            <td className="px-3 text-right">{formatNumber(mintedSupply)}</td>
-          </tr>
-          <tr>
-            <td>Emitted so far</td>
-            <td className="px-3 text-right">{formatNumber(emittedSupply)}</td>
-          </tr>
-          <tr>
-            <td>Percent emitted</td>
-            <td className="px-3 text-right">{formatNumber(Number(emittedSupply) / Number(mintedSupply), { style: 'percent', minimumFractionDigits: 2, maximumFractionDigits: 2 })}</td>
-          </tr>
-          <tr>
-            <td>Circulation</td>
-            <td className="px-3 text-right">{formatNumber(circulation)}</td>
-          </tr>
-        </tbody>
-      </table>
+        <table className="w-full">
+          <tbody>
+            <tr>
+              <td>Minted</td>
+              <td className="px-3 text-right">{formatNumber(mintedSupply)}</td>
+            </tr>
+            <tr>
+              <td>Emitted so far</td>
+              <td className="px-3 text-right">{formatNumber(emittedSupply)}</td>
+            </tr>
+            <tr>
+              <td>Percent emitted</td>
+              <td className="px-3 text-right">{formatNumber(Number(emittedSupply) / Number(mintedSupply), { style: 'percent', minimumFractionDigits: 2, maximumFractionDigits: 2 })}</td>
+            </tr>
+            <tr>
+              <td>Circulation</td>
+              <td className="px-3 text-right">{formatNumber(circulation)}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
 
-      <p className="pt-5">&#128293;</p>
+      <div className="rounded-xl bg-purple-50 p-2 shadow-sm">
+        <div className="flex justify-center">
+          <h3 className="text-lg font-medium">&#128293;</h3>
+        </div>
 
-      <table>
-        <tbody>
-          <tr>
-            <td>Burned so far</td>
-            <td className="px-3 text-right">{formatNumber(burnedSoFar)}</td>
-          </tr>
-          <tr>
-            <td>Pending burn</td>
-            <td className="px-3 text-right">{formatNumber(pendingBurn)}</td>
-          </tr>
-          <tr>
-            <td>Total burned</td>
-            <td className="px-3 text-right">{formatNumber(totalBurned)}</td>
-          </tr>
-          <tr>
-            <td>Percent burned</td>
-            <td className="px-3 text-right">{formatNumber((Number(totalBurned) / Number(mintedSupply)), { style: 'percent', minimumFractionDigits: 2, maximumFractionDigits: 2 })}</td>
-          </tr>
-          <tr>
-            <td>Remaining supply</td>
-            <td className="px-3 text-right">{formatNumber(remainingSupply - pendingBurn)}</td>
-          </tr>
-        </tbody>
-      </table>
+        <table className="w-full">
+          <tbody>
+            <tr>
+              <td>Burned so far</td>
+              <td className="px-3 text-right">{formatNumber(burnedSoFar)}</td>
+            </tr>
+            <tr>
+              <td>Pending burn</td>
+              <td className="px-3 text-right">{formatNumber(pendingBurn)}</td>
+            </tr>
+            <tr>
+              <td>Total burned</td>
+              <td className="px-3 text-right">{formatNumber(totalBurned)}</td>
+            </tr>
+            <tr>
+              <td>Percent burned</td>
+              <td className="px-3 text-right">{formatNumber((Number(totalBurned) / Number(mintedSupply)), { style: 'percent', minimumFractionDigits: 2, maximumFractionDigits: 2 })}</td>
+            </tr>
+            <tr>
+              <td>Remaining supply</td>
+              <td className="px-3 text-right">{formatNumber(remainingSupply - pendingBurn)}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      
+      <div className="rounded-xl bg-purple-50 p-2 shadow-sm">
+        <div className="flex justify-center">
+          <h3 className="text-lg font-medium">&#128274;</h3>
+        </div>
 
-      <p className="pt-5">&#128274;</p>
-
-      <table>
-        <tbody>
-          <tr>
-            <td>veDUST locked</td>
-            <td className="px-3 text-right">{formatNumber(locked)}</td>
-          </tr>
-          <tr>
-            <td>veDUST power</td>
-            <td className="px-3 text-right">{formatNumber(power)}</td>
-          </tr>
-        </tbody>
-      </table>
+        <table className="w-full">
+          <tbody>
+            <tr>
+              <td>veDUST locked</td>
+              <td className="px-3 text-right">{formatNumber(locked)}</td>
+            </tr>
+            <tr>
+              <td>veDUST power</td>
+              <td className="px-3 text-right">{formatNumber(power)}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
 
       <LastUpdated timestamp={lastUpdate} />
-    </>
+    </div>
   );
 }
