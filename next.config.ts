bc9c82aa8cb590e6e524ace1ed6b3fd1520e1d9c
Jack Farrington <jack.farrington@gmail.com>
2026-01-19 10:05:16 -0600

    Added metrics route rewrite.

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   cacheComponents: true,
+
+  async rewrites() {
+    return [
+      {
+        source: '/metrics',
+        destination: '/api/metrics',
+      }
+    ];
+  }
 };
 
 export default nextConfig;
